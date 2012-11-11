9cc9887202b04f72eb7e7424b7c74362de7c1787
Dan Christensen <opello@opello.org>
2012-11-11 14:02:05 -0600

    LM75 sensor script used during talk.

---
+++ lm75.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+from smbus import SMBus
+import struct
+import time
+
+# LM75 i2c address:
+addr = 0x4F
+
+i2c1 = SMBus(1)
+
+
+def parse(t):
+   # 9 bits of resolution stored in 2 bytes
+   r = t >> 7
+   # MSB set?
+   if (t & 0x8000):
+      # one's complement
+      r = (~r & 0x1FF)
+      # two's complement
+      r = r - 1
+      # significance: means negative temp
+      r = -r
+
+   r = r / 2.0
+
+   return r
+
+def ctof(t):
+   return ((9.0/5.0) * parse(t)) + 32.0
+
+
+while True:
+   temp = i2c1.read_word_data(addr, 0)
+   tempA = temp & 0xFF;
+   tempB = (temp >> 8) & 0xFF
+   temp = (tempA << 8) | tempB
+
+   print "read: %04x = %.1f C (%.1f F)" % (temp, parse(temp), ctof(temp))
+   time.sleep(0.5)
+   #time.sleep(3)
+
+# test negative:
+# ex read f500
+#val = 0xf500
+#print "read: %04x = %f" % (val, parse(val))
